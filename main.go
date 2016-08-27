4f84471b894d76a8005318717c952bac7c606cbc
Jordan Day <jordanday444@gmail.com>
2016-08-26 20:31:43 -0500

    Init

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "github.com/FryDay/chip8/chip8"
+
+func main() {
+	chip8 := chip8.Chip8{}
+	//graphics
+	//input
+
+	chip8.Initialize()
+	//loadRom
+
+	//for {
+	//cycle
+	//if draw flag then draw
+	//set keys
+	//}
+}
